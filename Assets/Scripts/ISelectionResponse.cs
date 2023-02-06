bce14d902802a0dd1b618ffca963963b724f0f9d
Konrad Dudek <kdudek111@wp.pl>
2023-02-06 22:48:04 +0100

    Downloaded assets and added celection manager

---
+++ Assets/Scripts/ISelectionResponse.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public interface ISelectionResponse
+{
+    public void OnSelect();
+    public void OnDeselect();
+}
---
+++ Assets/Scripts/OutlineSelectionResponse.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class OutlineSelectionResponse : MonoBehaviour, ISelectionResponse
+{
+    public void OnSelect()
+    {
+        var outline = gameObject.GetComponent<Outline>();
+        if (outline != null) outline.OutlineWidth = 10;
+    }
+
+    public void OnDeselect()
+    {
+        var outline = gameObject.GetComponent<Outline>();
+        if (outline != null) outline.OutlineWidth = 0;
+    }
+}
