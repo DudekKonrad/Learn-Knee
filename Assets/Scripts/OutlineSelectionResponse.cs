cb932e141a987e33a8a8a532ddd9740a7f5140b6
Konrad Dudek <kdudek111@wp.pl>
2023-03-03 10:24:30 +0100

    Added "Clean setting asset" and "DoTween asset"

--- Assets/Scripts/OutlineSelectionResponse.cs
+++ Assets/Scripts/OutlineSelectionResponse.cs
@@ -1,16 +1,26 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class OutlineSelectionResponse : MonoBehaviour, ISelectionResponse
 {
     public void OnSelect()
     {
         var outline = gameObject.GetComponent<Outline>();
-        if (outline != null) outline.OutlineWidth = 10;
+        if (outline != null)
+        {
+            DOTween.To(() => outline.OutlineWidth, x => outline.OutlineWidth = x, 20, 1);
+            //outline.OutlineWidth = 10;
+        }
     }
 
     public void OnDeselect()
     {
         var outline = gameObject.GetComponent<Outline>();
-        if (outline != null) outline.OutlineWidth = 0;
+        if (outline != null)
+        {
+            DOTween.To(() => outline.OutlineWidth, x => outline.OutlineWidth = x, 0, 1);
+
+            //outline.OutlineWidth = 0;
+        }
     }
 }
